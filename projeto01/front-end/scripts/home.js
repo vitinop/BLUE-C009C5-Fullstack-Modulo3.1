11d4abcc59f63cbe13505275c2c78f6bf92d412e
Victor Luz <victor.fernando.moura@hotmail.com>
2021-10-04 20:14:17 -0300

    updates

--- projeto01/front-end/scripts/home.js
+++ projeto01/front-end/scripts/home.js
@@ -5,19 +5,19 @@ const paginaAtual = window.location.pathname;
 
 // Faz uma requisicao do tipo [GET] que recebe todas os games cadastrados.
 const getJogos = async () => {
-    const response = await fetch(urlApi); // faz um req do tipo [GET] para a api
-    // data = uma lista (array de objetos) com as tarefas pre cadastra 
-    const data = await response.json();
-    console.log(data);
-    if (paginaAtual == '/front-end/index.html') {
-      // iteramos esse array passando item por item e renderizandop na tela
-      data.map((jogo) => {
-        lista.insertAdjacentHTML('beforeend',
-          `<div class="card">
+  const response = await fetch(urlApi); // faz um req do tipo [GET] para a api
+  // data = uma lista (array de objetos) com as tarefas pre cadastra 
+  const data = await response.json();
+  console.log(data);
+  if (paginaAtual == '/front-end/index.html') {
+    // iteramos esse array passando item por item e renderizandop na tela
+    data.map((jogo) => {
+      lista.insertAdjacentHTML('beforeend',
+        `<div class="card">
        <section class=cardTemplate>
            <div class="cardEditButton">
               <a href="./edit.html?gameid=${jogo.id}">
-               <img src="./static/edit.png" alt="Capa do Jogo">
+               <img src="./static/edit.png" alt="Icone de editar">
                </a>
            </div>
            <div class="cardGameIMG">
@@ -26,18 +26,23 @@ const getJogos = async () => {
   
            <div>
                <p class="cardInfoNota">${jogo.nota}</p>
-               <h1> ${jogo.nome}</h1>
+               <h1 class="cardInfoNome"> ${jogo.nome}</h1>
                <p class="cardInfoGender">${jogo.genero}</h3>
+               <div class="cardStatusConclusao">
+                <p>Status:
+            <img src="./static/ok.png" alt="Icone de editar">
+           </p>
+           </div>
+           
            </div>
            
        </section>
        
        <script src="script.js"></script>
       `
-        )
-      })
-    }
+      )
+    })
   }
-  
-  getJogos();
-  
+}
+
+getJogos();
